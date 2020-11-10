05e9229c27ecfb5e577c578f6b5690f6d27f80b0
Jonathan Reichelt Gjertsen <jonathan.gjertsen@disruptive-technologies.com>
2020-11-10 15:22:47 +0100

    Initial commit of baud rate estimate measurement

---
+++ BaudRateEstimate.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+from saleae.range_measurements import DigitalMeasurer
+
+class BaudRateEstimate(DigitalMeasurer):
+    supported_measurements = ["baud_rate"]
+
+    def __init__(self, requested_measurements):
+        super().__init__(requested_measurements)
+        self.pulses = []
+        self.state = None
+        self.last_edge = None
+
+    def process_data(self, data):
+        for time, bitstate in data:
+            if self.state is not None or self.last_edge is not None and self.state != bitstate:
+                self.pulses.append(float(time - self.last_edge))
+            self.last_edge = time
+            self.state = bitstate
+
+    def measure(self):
+        return { "baud_rate": 1 / np.percentile(self.pulses, 1) }
---
+++ README.md
@@ -0,0 +1,8 @@
+# Baud rate estimate
+
+Estimates the baud rate of a digital signal.
+To use it, drag the measurement over a channel with some traffic.
+
+The baud rate is estimated from the 1% percentile of the pulse widths,
+so that a few glitches don't throw off the estimate.
+
